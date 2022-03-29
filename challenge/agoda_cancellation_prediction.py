f36228d342efe829a811134ff70fa5d69aea1441
elkysandor <elky.sandor@mail.huji.ac.il>
2022-03-29 21:08:22 +0300

    data challenge data preprocssing

--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -1,12 +1,289 @@
 from challenge.agoda_cancellation_estimator import AgodaCancellationEstimator
 from IMLearn.utils import split_train_test
-from sklearn.linear_model import LinearRegression()
-from sklearn.tree import DecisionTreeClassifier()
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+import re
 
 import numpy as np
 import pandas as pd
 
+COUNTRY_ALPHA2_TO_CONTINENT = {
+    "TL": "Asia",
+    'AB': 'Asia',
+    'AD': 'Europe',
+    'AE': 'Asia',
+    'AF': 'Asia',
+    'AG': 'North America',
+    'AI': 'North America',
+    'AL': 'Europe',
+    'AM': 'Asia',
+    'AO': 'Africa',
+    'AR': 'South America',
+    'AS': 'Oceania',
+    'AT': 'Europe',
+    'AU': 'Oceania',
+    'AW': 'North America',
+    'AX': 'Europe',
+    'AZ': 'Asia',
+    'BA': 'Europe',
+    'BB': 'North America',
+    'BD': 'Asia',
+    'BE': 'Europe',
+    'BF': 'Africa',
+    'BG': 'Europe',
+    'BH': 'Asia',
+    'BI': 'Africa',
+    'BJ': 'Africa',
+    'BL': 'North America',
+    'BM': 'North America',
+    'BN': 'Asia',
+    'BO': 'South America',
+    'BQ': 'North America',
+    'BR': 'South America',
+    'BS': 'North America',
+    'BT': 'Asia',
+    'BV': 'Antarctica',
+    'BW': 'Africa',
+    'BY': 'Europe',
+    'BZ': 'North America',
+    'CA': 'North America',
+    'CC': 'Asia',
+    'CD': 'Africa',
+    'CF': 'Africa',
+    'CG': 'Africa',
+    'CH': 'Europe',
+    'CI': 'Africa',
+    'CK': 'Oceania',
+    'CL': 'South America',
+    'CM': 'Africa',
+    'CN': 'Asia',
+    'CO': 'South America',
+    'CR': 'North America',
+    'CU': 'North America',
+    'CV': 'Africa',
+    'CW': 'North America',
+    'CX': 'Asia',
+    'CY': 'Asia',
+    'CZ': 'Europe',
+    'DE': 'Europe',
+    'DJ': 'Africa',
+    'DK': 'Europe',
+    'DM': 'North America',
+    'DO': 'North America',
+    'DZ': 'Africa',
+    'EC': 'South America',
+    'EE': 'Europe',
+    'EG': 'Africa',
+    'ER': 'Africa',
+    'ES': 'Europe',
+    'ET': 'Africa',
+    'FI': 'Europe',
+    'FJ': 'Oceania',
+    'FK': 'South America',
+    'FM': 'Oceania',
+    'FO': 'Europe',
+    'FR': 'Europe',
+    'GA': 'Africa',
+    'GB': 'Europe',
+    'GD': 'North America',
+    'GE': 'Asia',
+    'GF': 'South America',
+    'GG': 'Europe',
+    'GH': 'Africa',
+    'GI': 'Europe',
+    'GL': 'North America',
+    'GM': 'Africa',
+    'GN': 'Africa',
+    'GP': 'North America',
+    'GQ': 'Africa',
+    'GR': 'Europe',
+    'GS': 'South America',
+    'GT': 'North America',
+    'GU': 'Oceania',
+    'GW': 'Africa',
+    'GY': 'South America',
+    'HK': 'Asia',
+    'HM': 'Antarctica',
+    'HN': 'North America',
+    'HR': 'Europe',
+    'HT': 'North America',
+    'HU': 'Europe',
+    'ID': 'Asia',
+    'IE': 'Europe',
+    'IL': 'Asia',
+    'IM': 'Europe',
+    'IN': 'Asia',
+    'IO': 'Asia',
+    'IQ': 'Asia',
+    'IR': 'Asia',
+    'IS': 'Europe',
+    'IT': 'Europe',
+    'JE': 'Europe',
+    'JM': 'North America',
+    'JO': 'Asia',
+    'JP': 'Asia',
+    'KE': 'Africa',
+    'KG': 'Asia',
+    'KH': 'Asia',
+    'KI': 'Oceania',
+    'KM': 'Africa',
+    'KN': 'North America',
+    'KP': 'Asia',
+    'KR': 'Asia',
+    'KW': 'Asia',
+    'KY': 'North America',
+    'KZ': 'Asia',
+    'LA': 'Asia',
+    'LB': 'Asia',
+    'LC': 'North America',
+    'LI': 'Europe',
+    'LK': 'Asia',
+    'LR': 'Africa',
+    'LS': 'Africa',
+    'LT': 'Europe',
+    'LU': 'Europe',
+    'LV': 'Europe',
+    'LY': 'Africa',
+    'MA': 'Africa',
+    'MC': 'Europe',
+    'MD': 'Europe',
+    'ME': 'Europe',
+    'MF': 'North America',
+    'MG': 'Africa',
+    'MH': 'Oceania',
+    'MK': 'Europe',
+    'ML': 'Africa',
+    'MM': 'Asia',
+    'MN': 'Asia',
+    'MO': 'Asia',
+    'MP': 'Oceania',
+    'MQ': 'North America',
+    'MR': 'Africa',
+    'MS': 'North America',
+    'MT': 'Europe',
+    'MU': 'Africa',
+    'MV': 'Asia',
+    'MW': 'Africa',
+    'MX': 'North America',
+    'MY': 'Asia',
+    'MZ': 'Africa',
+    'NA': 'Africa',
+    'NC': 'Oceania',
+    'NE': 'Africa',
+    'NF': 'Oceania',
+    'NG': 'Africa',
+    'NI': 'North America',
+    'NL': 'Europe',
+    'NO': 'Europe',
+    'NP': 'Asia',
+    'NR': 'Oceania',
+    'NU': 'Oceania',
+    'NZ': 'Oceania',
+    'OM': 'Asia',
+    'OS': 'Asia',
+    'PA': 'North America',
+    'PE': 'South America',
+    'PF': 'Oceania',
+    'PG': 'Oceania',
+    'PH': 'Asia',
+    'PK': 'Asia',
+    'PL': 'Europe',
+    'PM': 'North America',
+    'PR': 'North America',
+    'PS': 'Asia',
+    'PT': 'Europe',
+    'PW': 'Oceania',
+    'PY': 'South America',
+    'QA': 'Asia',
+    'RE': 'Africa',
+    'RO': 'Europe',
+    'RS': 'Europe',
+    'RU': 'Europe',
+    'RW': 'Africa',
+    'SA': 'Asia',
+    'SB': 'Oceania',
+    'SC': 'Africa',
+    'SD': 'Africa',
+    'SE': 'Europe',
+    'SG': 'Asia',
+    'SH': 'Africa',
+    'SI': 'Europe',
+    'SJ': 'Europe',
+    'SK': 'Europe',
+    'SL': 'Africa',
+    'SM': 'Europe',
+    'SN': 'Africa',
+    'SO': 'Africa',
+    'SR': 'South America',
+    'SS': 'Africa',
+    'ST': 'Africa',
+    'SV': 'North America',
+    'SY': 'Asia',
+    'SZ': 'Africa',
+    'TC': 'North America',
+    'TD': 'Africa',
+    'TG': 'Africa',
+    'TH': 'Asia',
+    'TJ': 'Asia',
+    'TK': 'Oceania',
+    'TM': 'Asia',
+    'TN': 'Africa',
+    'TO': 'Oceania',
+    'TP': 'Asia',
+    'TR': 'Asia',
+    'TT': 'North America',
+    'TV': 'Oceania',
+    'TW': 'Asia',
+    'TZ': 'Africa',
+    'UA': 'Europe',
+    'UG': 'Africa',
+    'US': 'North America',
+    'UY': 'South America',
+    'UZ': 'Asia',
+    'VC': 'North America',
+    'VE': 'South America',
+    'VG': 'North America',
+    'VI': 'North America',
+    'VN': 'Asia',
+    'VU': 'Oceania',
+    'WF': 'Oceania',
+    'WS': 'Oceania',
+    'XK': 'Europe',
+    'YE': 'Asia',
+    'YT': 'Africa',
+    'ZA': 'Africa',
+    'ZM': 'Africa',
+    'ZW': 'Africa',
+    "A1": 'Unknown',
+    np.nan: "Unknown"
+}
 
+def fillter_to_binary(val):
+    if val in [0,1,1.0,0.0] or np.isnan(val):
+        return True
+    return False
+def prase_to_vec(lst):
+    vec=np.zeros(2)
+    if lst:
+        for i in lst:
+            before_D = re.findall(r"(\d+)D", i)
+            before_N = re.findall(r"(\d+)N", i)
+            before_P = re.findall(r"(\d+)P", i)
+        if before_D:
+            vec[0] = (np.array(before_D).astype(int)).mean()
+        if before_N:
+            vec[1] = (np.array(before_N).astype(int)).mean()
+        return vec,np.array(before_P)
+    return lst
+
+def counry_code_to_continent(contry):
+    return COUNTRY_ALPHA2_TO_CONTINENT[contry]
+
+def remove_not_showing(lst):
+    return [strr for strr in lst if "D" in strr]
+
+def change_to_int(lst):
+    return
 def load_data(filename: str):
     """
     Load Agoda booking cancellation dataset
@@ -23,10 +300,35 @@ def load_data(filename: str):
     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
     """
     # TODO - replace below code with any desired preprocessing
-    full_data = pd.read_csv(filename).dropna().drop_duplicates().drop(columns="h_booking_id",inplace=True)
+    full_data = pd.read_csv(filename).drop_duplicates()
     full_data.drop(columns=["h_booking_id"],axis=1,inplace=True)
-    full_data.cancellation_datetime =full_data.cancellation_datetime.astype(bool).astype(int)
-    binary_data = full_data.columns[full_data.isin([0,1,0.0,1.0,np.nan]).all()]
+    full_data.cancellation_datetime = full_data.cancellation_datetime.fillna(0).astype(bool).astype(int)
+    binary_data = full_data.select_dtypes([np.number]).columns[full_data.select_dtypes([np.number]).applymap(fillter_to_binary).all()]
+    full_data["continent"] = full_data.origin_country_code.apply(counry_code_to_continent)
+    # we saw that num of rooms has high corr with num of adults so we can keep one of them.
+    str_vec = full_data.cancellation_policy_code.str.split("_")
+    str_vec = str_vec.apply(remove_not_showing)
+
+    def __str_to_numeric(s):
+        number_string = ''
+        for c in s:
+            number_string += str(ord(c)).zfill(3)
+
+    date_time_cols = ['booking_datetime', 'checkin_date', 'checkout_date',
+                      'hotel_live_date', 'cancellation_datetime']
+    #original_payment_method, origin_country_code&guest_nationality_country_name, charge_option - add as features
+    names_of_non_numeric_cols = ['hotel_country_code', 'accommadation_type_name',
+                                 'charge_option', 'customer_nationality',
+                                 'guest_nationality_country_name', 'origin_country_code', 'language',
+                                 'original_payment_method', 'original_payment_type',
+                                 'original_payment_currency', 'cancellation_policy_code']
+    #looks like pyment method is not so informative becuase it is corralte with the comment ones
+
+    for date_time_col_name in date_time_cols:
+        full_data[date_time_col_name] = pd.to_datetime(full_data[date_time_col_name]).view(np.int64)
+
+    for non_numeric_col_name in names_of_non_numeric_cols:
+        full_data[non_numeric_col_name] = full_data[non_numeric_col_name].apply(lambda x: __str_to_numeric(x))
 
     features = full_data[["h_booking_id",
                           "hotel_id",
